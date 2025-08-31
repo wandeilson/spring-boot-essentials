11235cf7a0af3773c9c00040e23f9437bcff31dc
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2025-08-31 16:25:28 -0300

    aula 04

--- src/main/java/silva/wandeilson/springboot2/ApplicationStart.java
+++ src/main/java/silva/wandeilson/springboot2/ApplicationStart.java
@@ -1,11 +1,12 @@
-package silva.wandeilson.springboot2.start;
+package silva.wandeilson.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "silva.wandeilson.springboot2")
+@SpringBootApplication
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
--- src/main/java/silva/wandeilson/springboot2/controller/AnimeController.java
+++ src/main/java/silva/wandeilson/springboot2/controller/AnimeController.java
@@ -1,18 +1,29 @@
 package silva.wandeilson.springboot2.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import silva.wandeilson.springboot2.domain.Anime;
+import silva.wandeilson.springboot2.util.DateUtil;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+
+    private final DateUtil dateUtil;
 
     @GetMapping(path = "list")
     public List<Anime> list(){
+        log.info("Data Hora-> " + dateUtil.formartLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Berserk"));
     }
 
